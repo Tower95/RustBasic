e3d829e1d191242cfdaaffc150e8a2d9c99fcb1b
Tower95 <torrestmi3@gmail.com>
2022-11-07 06:38:45 -0700

    Loop

--- src/main.rs
+++ src/main.rs
@@ -64,25 +64,61 @@ fn main() {
     //     println!("A tu casa a ver pocoyo... PATO");
     // }
 
-    let mut eleccion : String = String::new();
-    
-    println!("Esta es tu última oportunidad. Después, ya no podrás echarte atrás. Si tomas la pastilla azul, fin de la historia. Despertarás en tu cama y creerás lo que quieras creerte. Si tomas la roja, te quedas en el País de las Maravillas y yo te enseñaré hasta dónde llega la madriguera de conejos. Recuerda lo único que te ofrezco es la verdad. Nada más.");
-    println!("Qué pastilla tomarás? roja o azul?");
+    // let mut eleccion : String = String::new();
 
-    std::io::stdin().read_line(&mut eleccion).unwrap();
+    // println!("Esta es tu última oportunidad. Después, ya no podrás echarte atrás. Si tomas la pastilla azul, fin de la historia. Despertarás en tu cama y creerás lo que quieras creerte. Si tomas la roja, te quedas en el País de las Maravillas y yo te enseñaré hasta dónde llega la madriguera de conejos. Recuerda lo único que te ofrezco es la verdad. Nada más.");
+    // println!("Qué pastilla tomarás? roja o azul?");
 
-    eleccion.trim().to_string();
+    // std::io::stdin().read_line(&mut eleccion).unwrap();
 
-    let pastilla : &str = &eleccion.trim();
+    // eleccion.trim().to_string();
 
-    if pastilla == "roja" {
-        println!("Despretaste en tu casa");
-    } 
-    else if pastilla == "azul" {
-        println!("Bueno dorty de Kansas puedes despedirte...");
-    } else{
-        println!("Knock knock despierta NEO!!!");
+    // let pastilla : &str = &eleccion.trim();
+
+    // if pastilla == "roja" {
+    //     println!("Despretaste en tu casa");
+    // }
+    // else if pastilla == "azul" {
+    //     println!("Bueno dorty de Kansas puedes despedirte...");
+    // } else{
+    //     println!("Knock knock despierta NEO!!!");
+    // }
+
+    // Ciclo  Loop
+
+    //Creamos los dos numeros que vamos a crear
+    let primer_numero: i16 = 102;
+    let  segundo_numero: i16 = 32;
+    let suma = primer_numero + segundo_numero;
+
+    // Creacion del loop
+
+    loop { // Itera esto asta que el usuario asierte
+        // mostrar los dos numeros
+
+        println!(
+            "Cual es el resultado de sumar\n{} + {}",
+            primer_numero, segundo_numero
+        );
+
+        //capturar respuesta del ususario
+
+        let mut respuesta: String = String::new();
+        std::io::stdin().read_line(&mut respuesta).unwrap();
+
+        let  respuesta_in: i16 = respuesta.trim().parse().unwrap();
+
+        // Comparamos los resultados
+        if respuesta_in == suma {
+            println!("Tu respuesta: {} es CORRECTA", respuesta);
+            //termina el loop
+            break;
+
+        } else {
+            println!(
+                "Tu respuesta: {} es INCORRECTA intentalo de nuevo",
+                respuesta
+            );
+        }
     }
-
-
 }
