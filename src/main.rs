a3fa42535f6acbe1f2841974141fb9c6518f8ff1
Tower95 <torrestmi3@gmail.com>
2022-11-07 05:52:27 -0700

    simple form Name country

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,43 @@ fn main() {
     // println!("Hola soy {} y tengo {} años",nombre, edad);
     // println!("En mi ciudad la temperatura maxima de este año fue de: {}\n y la temperatura minima fue: {}", temperatura_max,temperatura_min);
 
-    // Introdocir datos
-    println!("Porfavor introduce tu nomnbre...");
+    // // Introdocir datos
+    // println!("Porfavor introduce tu nomnbre...");
 
-    let mut nombre: String  = String::new(); 
+    // let mut nombre: String  = String::new(); 
     
-    std::io::stdin().read_line(&mut nombre ).unwrap();
+    // std::io::stdin().read_line(&mut nombre ).unwrap();
+
+    // nombre = nombre.trim().to_string();
+
+    // // Optener la edad de la consola y convertirla en un numero
+    // println!("Porfavor introduce tu edad...");
+    
+    // let mut edad : String  = String::new(); 
+    
+    // std::io::stdin().read_line(&mut edad ).unwrap();
+
+    // let edad_in : u8 = edad.trim().parse().unwrap();
+    
+    // println!("Hola bienvenido {} de {} años", nombre, edad_in);
+
+    let mut nombre : String = String::new();
+    let mut pais: String = String::new();
+
+    println!("Hola bienvenido porfavor indique su nombre completo...");
+
+    std::io::stdin().read_line(&mut nombre).unwrap();
 
     nombre = nombre.trim().to_string();
 
-    // Optener la edad de la consola y convertirla en un numero
-    println!("Porfavor introduce tu edad...");
-    
-    let mut edad : String  = String::new(); 
-    
-    std::io::stdin().read_line(&mut edad ).unwrap();
 
-    let edad_in : u8 = edad.trim().parse().unwrap();
-    
-    println!("Hola bienvenido {} de {} años", nombre, edad_in);
+    println!("indique su pais de recidencia...");
 
+    std::io::stdin().read_line(&mut pais).unwrap();
+    
+    pais = pais.trim().to_string();
+
+    println!("Tu nomnbre es {} y vives en {}", nombre, pais);
 
     
 }
