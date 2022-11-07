b7aaa650849dacec4d388ffaa5b9a33a31eb0251
Tower95 <torrestmi3@gmail.com>
2022-11-07 05:43:01 -0700

    introducing data from terminal

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    // declaracion de variables
+    // let mut edad: u8 = 27;
+    // let nombre: &str = "Ismael Torres";
+    // edad = edad+1;
+
+    // let temperatura_max: i8 = 40;
+    // let temperatura_min: i8 = -15;
+
+    // println!("Hola soy {} y tengo {} años",nombre, edad);
+    // println!("En mi ciudad la temperatura maxima de este año fue de: {}\n y la temperatura minima fue: {}", temperatura_max,temperatura_min);
+
+    // Introdocir datos
+    println!("Porfavor introduce tu nomnbre...");
+
+    let mut nombre: String  = String::new(); 
+    
+    std::io::stdin().read_line(&mut nombre ).unwrap();
+
+    nombre = nombre.trim().to_string();
+
+    // Optener la edad de la consola y convertirla en un numero
+    println!("Porfavor introduce tu edad...");
+    
+    let mut edad : String  = String::new(); 
+    
+    std::io::stdin().read_line(&mut edad ).unwrap();
+
+    let edad_in : u8 = edad.trim().parse().unwrap();
+    
+    println!("Hola bienvenido {} de {} años", nombre, edad_in);
+
+
+    
+}
+
